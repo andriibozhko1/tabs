3531273f3d2c3c68a25230255f1d109c33d0e769
Andrii <andriibozhko1@gmail.com>
2018-11-14 22:11:50 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# tabs
+# Hello World
+## This is my Github-Pages with my Glup - https://andriibozhko1.github.io/gulp/dist/
---
+++ dist/js/js.js
@@ -0,0 +1,18 @@
+const btn = document.querySelectorAll('.tabs-container__btn');
+const tabs = document.querySelectorAll('.tabs-block');
+
+(function() {
+  for (let i = 0; btn[i]; i++) {
+    btn[i].addEventListener("click", function() {
+      for (let j = 0; tabs[j]; j++) {
+        if (btn[i].value == j) {
+          tabs[j].classList.remove("none");
+          btn[j].style.border = '1px solid silver';
+        } else {
+          tabs[j].className += " none";
+          btn[j].style.border = 'none';
+        }
+      }
+    });
+  }
+})();
