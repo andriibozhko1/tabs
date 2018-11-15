943f37c2f35ef76077c1cf8e94704395b861d29f
Andrii <andriibozhko1@gmail.com>
2018-11-15 15:52:55 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,17 @@
-const btn = document.querySelectorAll('.tabs-container__btn');
-const tabs = document.querySelectorAll('.tabs-block');
-
-(function() {
-  for (let i = 0; btn[i]; i++) {
-    btn[i].addEventListener("click", function() {
-      for (let j = 0; tabs[j]; j++) {
-        if (btn[i].value == j) {
-          tabs[j].classList.remove("none");
-          btn[j].style.border = '1px solid silver';
+(function () {
+  const tabs = document.querySelectorAll('.tabs-container__item');
+  const mainBlock = document.querySelectorAll('.tabs-container__main-content');
+  for (let i = 0; tabs[i]; i++) {
+    tabs[i].addEventListener('click', () => {
+      for (let j = 0; mainBlock[j]; j++) {
+        if (tabs[i].dataset.tabs == mainBlock[j].dataset.content) {
+          mainBlock[j].classList.remove('hide');
+          tabs[j].classList.add('active');
         } else {
-          tabs[j].classList.add("none");
-          btn[j].style.border = 'none';
+          mainBlock[j].classList.add('hide');
+          tabs[j].classList.remove('active');
         }
       }
     });
   }
-})();
+}());
